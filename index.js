251fde11e2b86bcccb03db35d4e4cde7a81c1d19
putla venkat praneeth <putlavenkat123@gmail.com>
2024-10-25 23:16:00 +0530

    changes

---
+++ index.js
@@ -0,0 +1,50 @@
+var light="red";
+ if(light=="red"){
+  console.log("stop");
+ }
+ else if(light=="green"){
+  console.log("go");
+ }
+ else if(light=="yellow"){
+  console.log(" get ready to move");
+ }
+ else {console.log("invalid ip");}
+
+
+var temp="0";
+if(temp=="0"){
+    console.log("its freezing cold");
+}
+else if(temp=="15"){
+    console.log("its a cool day");
+}
+else if(temp=="25"){
+    console.log("its a pleasant day");
+}
+else{console.log("unknown weather");}
+
+
+
+var number="1";
+if(number=="1"){
+    console.log("monday")
+}
+else if(number=="2"){
+    console.log("tuesday");
+}
+else if(number=="3"){
+    console.log("wednesday");
+}
+else if(number=="4"){
+    console.log("thursday");
+}
+else if(number=="5"){
+    console.log("friday");
+}
+else if(number=="6"){
+    console.log("satursday");
+}
+else if(number=="7"){
+    console.log("sunday");
+}
+else{console.log("invalid day number");}
